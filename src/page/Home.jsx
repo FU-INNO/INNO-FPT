32b31171f0f9aa8fc27197583d0ecbae7fcab1db
Saugao274 <thuynttde170035@fpt.edu.vn>
2024-07-10 20:52:18 +0700

    home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,15 @@
+import { Image } from "antd";
+import Banner from "../components/Banner";
+import VisonHome from "../components/VisonHome";
+import dreamImg from "../assets/About_fptedu.jpg";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col justify-center">
+      <Banner />
+      <VisonHome />
+      <Image src={dreamImg} />
+    </div>
+  );
 };
 
 export default Home;
