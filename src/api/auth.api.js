031c67812f9293afe55c81a59090eafb755e629f
josphanai0203 <Kaidophan37@gmail.com>
2024-07-11 23:50:42 +0700

    login

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const login = async ({ id, token }) => {
-  return await axios.post("http://localhost:8080/v1/api/auth/login", { id, tokenLogin: token });
+  return await axios.post("/v1/api/auth/login", { id, tokenLogin: token });
 };
