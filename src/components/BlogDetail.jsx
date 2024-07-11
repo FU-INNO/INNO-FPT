8252bd67878b26e959d45f0ed09a3575b52e56c8
Đào Bá Anh Quân <quandba154@gmail.com>
2024-07-12 00:02:41 +0700

    11/7 phuong 2

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -1,167 +1,94 @@
-import { Row, Col, Typography, Tag, Image, Avatar, Input, Button } from 'antd';
-import { HeartOutlined, FacebookOutlined, TwitterOutlined, PinterestOutlined, LinkedinOutlined } from '@ant-design/icons';
-import "../styles/DetailBlog.css";
+import { Row, Col, Typography, Tag, Image, Avatar, Input, Button } from "antd";
+import {
+  HeartOutlined,
+  FacebookOutlined,
+  TwitterOutlined,
+  PinterestOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 
 const { Title, Text, Paragraph } = Typography;
 const { TextArea } = Input;
 
 const DetailBlog = () => {
-    return (
-        <div className="single-post" style={{ width: '100%' }}>
-            <div className="post-header mb-5 text-center">
-                <div className="meta-cat">
-                    <Tag className="font-extra text-color text-uppercase font-sm letter-spacing-1">Health</Tag>
-                    <Tag className="font-extra text-color text-uppercase font-sm letter-spacing-1">Lifestyle</Tag>
-                </div>
-                <Title level={2} className="post-title mt-2">First Look At Autumn Collection</Title>
-                <div className="post-meta">
-                    <Text className="text-uppercase font-sm letter-spacing-1 mr-3">by Liam</Text>
-                    <Text className="text-uppercase font-sm letter-spacing-1">January 17, 2019</Text>
-                </div>
-                <div className="post-featured-image mt-5">
-                    <Image src="images/fashion/bg-banner.jpg" alt="featured-image" style={{ width: '100%' }} />
-                </div>
-            </div>
+  return (
+    <div className="w-full">
+      <div className="mb-5 text-center">
+        <Title level={2} className="mt-2">
+          First Look At Autumn Collection
+        </Title>
+      </div>
 
-            <div className="post-body">
-                <div className="entry-content">
-                    <Paragraph>dssfsfddsfsdsdf.</Paragraph>
-                    <Title level={2} className="mt-4 mb-3">sfdsdkfsdfl</Title>
-                    <Paragraph>fdsjhdfgjgdbnvdbvb</Paragraph>
-                    <blockquote>
-                        <i className="ti-quote-left mr-2"></i>sfgjdgsfnfng.<i className="ti-quote-right ml-2"></i>
-                    </blockquote>
-                    <Row gutter={[16, 16]}>
-                        <Col lg={12} md={12}>
-                            <Image src="images/fashion/single-img1.png" alt="post-ads" style={{ width: '100%' }} />
-                        </Col>
-                        <Col lg={12} md={12}>
-                            <Image src="images/fashion/single-img2.png" alt="post-ads" style={{ width: '100%' }} />
-                        </Col>
-                    </Row>
-                    <Title level={3} className="mt-5 mb-3">dfksfgesoriroi</Title>
-                    <Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde cum delectus exercitationem natus quidem enim error suscipit. Iure cupiditate nobis quaerat consectetur! Vero aliquam, amet ipsum ullam reiciendis nostrum voluptate accusantium provident ut blanditiis incidunt.</Paragraph>
-                    <Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ab ratione animi nobis in et consequatur earum modi repellendus, qui, non debitis pariatur tempora consequuntur!</Paragraph>
-                </div>
-
-                <div className="post-tags py-4">
-                    <Tag>#Health</Tag>
-                    <Tag>#Game</Tag>
-                    <Tag>#Tour</Tag>
-                </div>
-
-                <div className="tags-share-box center-box d-flex text-center justify-content-between border-top border-bottom py-3">
-                    <span className="single-comment-o"><i className="fa fa-comment-o"></i>0 comment</span>
-                    <div className="post-share">
-                        <span className="count-number-like">2</span>
-                        <HeartOutlined className="single-like-button" />
-                    </div>
-                    <div className="list-posts-share">
-                        <a target="_blank" rel="nofollow" href="#"><FacebookOutlined /></a>
-                        <a target="_blank" rel="nofollow" href="#"><TwitterOutlined /></a>
-                        <a target="_blank" rel="nofollow" href="#"><PinterestOutlined /></a>
-                        <a target="_blank" rel="nofollow" href="#"><LinkedinOutlined /></a>
-                    </div>
-                </div>
-            </div>
-
-            <div className="post-author d-flex my-5">
-                <div className="author-img">
-                    <Avatar src="images/author.jpg" size={100} />
-                </div>
-                <div className="author-content pl-4">
-                    <Title level={4} className="mb-3">
-                        <a href="#" title="" rel="author" className="text-capitalize">Themefisher</a>
-                    </Title>
-                    <Paragraph>erbrtgj34p</Paragraph>
-                    <div className="author-social">
-                        <a target="_blank" href="#"><FacebookOutlined /></a>
-                        <a target="_blank" href="#"><TwitterOutlined /></a>
-                        <a target="_blank" href="#"><PinterestOutlined /></a>
-                        <a target="_blank" href="#"><LinkedinOutlined /></a>
-                    </div>
-                </div>
-            </div>
-
-            <nav className="post-pagination clearfix border-top border-bottom py-4">
-                <div className="prev-post">
-                    <a href="blog-single.html">
-                        <span className="text-uppercase font-sm letter-spacing">Next</span>
-                        <Title level={4} className="mt-3">rtgnjrstrt</Title>
-                    </a>
-                </div>
-                <div className="next-post">
-                    <a href="blog-single.html">
-                        <span className="text-uppercase font-sm letter-spacing">Previous</span>
-                        <Title level={4} className="mt-3">fsgdosgjo</Title>
-                    </a>
-                </div>
-            </nav>
-
-            <div className="related-posts-block mt-5">
-                <Title level={3} className="news-title mb-4 text-center">You May Also Like</Title>
-                <Row gutter={[16, 16]}>
-                    <Col lg={8} md={12} sm={24}>
-                        <div className="post-block-wrapper mb-4 mb-lg-0">
-                            <a href="blog-single.html">
-                                <Image src="images/fashion/img-1.jpg" alt="post-thumbnail" style={{ width: '100%' }} />
-                            </a>
-                            <div className="post-content mt-3">
-                                <Title level={5}>
-                                    <a href="blog-single.html">rausueotgsjb</a>
-                                </Title>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col lg={8} md={12} sm={24}>
-                        <div className="post-block-wrapper mb-4 mb-lg-0">
-                            <a href="blog-single.html">
-                                <Image src="images/fashion/img-2.jpg" alt="post-thumbnail" style={{ width: '100%' }} />
-                            </a>
-                            <div className="post-content mt-3">
-                                <Title level={5}>
-                                    <a href="blog-single.html">dsnfgesorjgtort</a>
-                                </Title>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col lg={8} md={12} sm={24}>
-                        <div className="post-block-wrapper">
-                            <a href="blog-single.html">
-                                <Image src="images/fashion/img-3.jpg" alt="post-thumbnail" style={{ width: '100%' }} />
-                            </a>
-                            <div className="post-content mt-3">
-                                <Title level={5}>
-                                    <a href="blog-single.html">dgjsogjettrg</a>
-                                </Title>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-
-            <form className="comment-form mb-5 gray-bg p-5" id="comment-form">
-                <Title level={3} className="mb-4 text-center">Leave a comment</Title>
-                <Row gutter={[16, 16]}>
-                    <Col span={24}>
-                        <TextArea className="form-control mb-3" name="comment" id="comment" rows={5} placeholder="Comment" />
-                    </Col>
-                    <Col span={12}>
-                        <Input className="form-control mb-3" name="name" id="name" placeholder="Name" />
-                    </Col>
-                    <Col span={12}>
-                        <Input className="form-control mb-3" name="email" id="email" placeholder="Email" />
-                    </Col>
-                    <Col span={24}>
-                        <Input className="form-control mb-3" name="website" id="website" placeholder="Website" />
-                    </Col>
-                    <Col span={24}>
-                        <Button type="primary" htmlType="submit">Post Comment</Button>
-                    </Col>
-                </Row>
-            </form>
+      <div>
+        <div>
+          {/* articleSection */}
+          <Title level={2} className="mt-4 mb-3">
+            {" "}
+            New Arrivals
+          </Title>
+          {/* articleSection */}
+          {/* name author */}
+          <div className="flex flex-row">
+            <Paragraph className="text-justify">Nguyen van a</Paragraph>
+            <Row lg={12} md={12}>
+              {/* anh tac gia */}
+              <Image
+                src="images/fashion/single-img1.png"
+                alt="post-ads"
+                className="w-full" //hight và width DB
+              />
+            </Row>
+          </div>
+          {/* name author */}
+          {/* description */}
+          <blockquote className="italic border-l-4 border-gray-500 pl-4">
+            <i className="ti-quote-left mr-2"></i>
+            "Fashion is the armor to survive the reality of everyday life."
+            <i className="ti-quote-right ml-2"></i>
+          </blockquote>
+          <Row gutter={[16, 16]}>
+            <Col lg={12} md={12}>
+              <Image
+                src="images/fashion/single-img2.png"
+                alt="post-ads"
+                className="w-full"
+              />
+            </Col>
+          </Row>
         </div>
-    );
+
+        <div className="py-4">
+          <Tag>#Health</Tag>
+          <Tag>#Lifestyle</Tag>
+          <Tag>#Fashion</Tag>
+        </div>
+
+        <div className="flex justify-between items-center border-t border-b py-3">
+          <span className="flex items-center">
+            <i className="fa fa-comment-o"></i> 0 comments
+          </span>
+          <div className="flex items-center">
+            <span className="mr-2">2</span>
+            <HeartOutlined className="ml-2" />
+          </div>
+          <div className="flex space-x-3">
+            <a target="_blank" rel="nofollow" href="#">
+              <FacebookOutlined />
+            </a>
+            <a target="_blank" rel="nofollow" href="#">
+              <TwitterOutlined />
+            </a>
+            <a target="_blank" rel="nofollow" href="#">
+              <PinterestOutlined />
+            </a>
+            <a target="_blank" rel="nofollow" href="#">
+              <LinkedinOutlined />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailBlog;
--- src/page/DetailBlog.jsx
+++ src/page/DetailBlog.jsx
@@ -1,27 +1,75 @@
-import { Row, Col } from 'antd';
+import { Row, Col } from "antd";
 import BlogDetail from "../components/BlogDetail";
 import AsideBlog from "../components/AsideBlog";
 import BannerHistory from "../components/BannerHistory";
 import "../styles/DetailBlog.css";
 
 const BlogPost = () => {
-    return (
-        <>
-         <BannerHistory />
-        <section className="single-block-wrapper section-padding" >
-            <div style={{ width: '100%', padding: '0 16px' }}>
-                <Row gutter={[16, 16]}>
-                    <Col xs={24} md={16}>
-                        <BlogDetail />
-                    </Col>
-                    <Col xs={24} md={8}>
-                        <AsideBlog />
-                    </Col>
-                </Row>
-            </div>
-        </section>
-        </>
-    );
-}
+  return (
+    <>
+      <BannerHistory />
+      <section className="single-block-wrapper section-padding">
+        <div style={{ width: "100%", padding: "0 16px" }}>
+          <Row gutter={[16, 16]}>
+            <Col xs={24} md={16}>
+              <BlogDetail />
+            </Col>
+            <Col xs={24} md={8}>
+              <AsideBlog />
+            </Col>
+          </Row>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default BlogPost;
+
+{
+  /* <form className="mb-5 bg-gray-100 p-5" id="comment-form">
+        <Title level={3} className="mb-4 text-center">
+          Leave a Comment
+        </Title>
+        <Row gutter={[16, 16]}>
+          <Col span={24}>
+            <TextArea
+              className="mb-3 w-full"
+              name="comment"
+              id="comment"
+              rows={5}
+              placeholder="Comment"
+            />
+          </Col>
+          <Col span={12}>
+            <Input
+              className="mb-3 w-full"
+              name="name"
+              id="name"
+              placeholder="Name"
+            />
+          </Col>
+          <Col span={12}>
+            <Input
+              className="mb-3 w-full"
+              name="email"
+              id="email"
+              placeholder="Email"
+            />
+          </Col>
+          <Col span={24}>
+            <Input
+              className="mb-3 w-full"
+              name="website"
+              id="website"
+              placeholder="Website"
+            />
+          </Col>
+          <Col span={24}>
+            <Button type="primary" htmlType="submit">
+              Post Comment
+            </Button>
+          </Col>
+        </Row>
+      </form> */
+}
