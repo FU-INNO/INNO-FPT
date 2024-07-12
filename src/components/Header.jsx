46729cf45c3468c87fe92d60d49345bbd4aae4ad
Đào Bá Anh Quân <quandba154@gmail.com>
2024-07-12 09:01:07 +0700

    12/7 2 quan 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,8 +36,8 @@ const Header = () => {
       link: "/question",
     },
     {
-      home: "Đề xuất dự án",
-      link: "/location",
+      home: "Game",
+      link: "/game",
     },
   ];
   const [isMobile, setIsMobile] = useState(false);
